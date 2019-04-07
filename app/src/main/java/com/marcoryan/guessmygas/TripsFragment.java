5f2a46207763914b46371e765515a4a1ec36457b
marco <marcoliang333@gmail.com>
2019-04-07 15:51:35 -0700

    added first card, hardcoded in messages, made it clickable with a test toast message.

--- app/src/main/java/com/marcoryan/guessmygas/TripsFragment.java
+++ app/src/main/java/com/marcoryan/guessmygas/TripsFragment.java
@@ -1,26 +1,31 @@
 package com.marcoryan.guessmygas;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-
+import android.widget.Toast;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
-public class TripsFragment extends Fragment {
 
+public class TripsFragment extends Fragment implements View.OnClickListener {
 
-    public TextView t;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.trips_fragment, container, false);
 
-        t = rootView.findViewById(R.id.TRIP_TEXT);
-        t.setText("TRIPS");
+        CardView card = rootView.findViewById(R.id.card1);
+        card.setOnClickListener(this);
 
         return rootView;
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(getContext(),"yes", Toast.LENGTH_SHORT).show();
     }
 
 }
